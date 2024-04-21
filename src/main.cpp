429c1b6ca92195968ce38482b0382caa30dffea7
juergs <juergen.schweiss@web.de>
2024-04-21 19:30:36 +0200

    better code layout

--- src/main.cpp
+++ src/main.cpp
@@ -277,27 +277,27 @@ void setup()
 
 }
 
-int angle=0;
-int values[20]={0};
-int values2[20]={0};
-int curent=0;
-int counter=0;
-int counter_last=2;
-int calib=20;
+int angle       = 0;
+int values[20]  = {0};
+int values2[20] = {0};
+int curent      = 0;
+int counter     = 0;
+int counter_last= 2;
+int calib       = 20;
 
-int curentTime=0;
-int period=500;
+int curentTime  = 0;
+int period      = 500;
 
-int term[6]={0};
-int term2[6]={0};
+int term[6]     = {0};
+int term2[6]    = {0};
 
-int t[6]={0};
-int t2[6]={0};
-bool c=0;
+int t[6]        = {0};
+int t2[6]       = {0};
+bool c          = 0;
 bool c1;
-int number=0;
-bool onOff=0;
-int debounce=0;
+int number      = 0;
+bool onOff      = 0;
+int debounce    = 0;
 
 //----------------------------------------------------
 
@@ -308,12 +308,12 @@ void loop()
  {
   if(debounce==0)
   {
-    debounce=1;
-    onOff=!onOff;
+    debounce  = 1;
+    onOff = !onOff;
     digitalWrite(LED,onOff);
     }
   }
-  else debounce=0;
+  else debounce = 0;
   
   angle   = map(analogRead(ADC1),0,4095,0,359);
   int num = analogRead(ADC2);
@@ -327,109 +327,183 @@ void loop()
   
   values[19]=curent;
 
-
- 
+  //--- button_label @ sprite4
   sprite4.fillRoundRect(0,0,40,80,4,color3);
+
+  //--- graph @ sprite2 
   sprite2.fillSprite(TFT_BLACK);
   
-  //sprite1.fillRoundRect(0,0,140,140,3,color6);
+  //--- text in gauge
   sprite1.fillRoundRect(80,200,140,140,3,color6);
   sprite1.setTextFont(0);
 
   for(int i=0;i<10;i++)
+  {
      if(start[i]+angle<360)
      {
-      sprite1.drawString(cc[i],x[start[i]+angle],y[start[i]+angle]);
-      sprite1.drawLine(px[start[i]+angle],py[start[i]+angle],lx[start[i]+angle],ly[start[i]+angle],color1);
-    }
-    else
-    {
-      sprite1.drawString(cc[i],x[(start[i]+angle)-360],y[(start[i]+angle)-360]);
-      sprite1.drawLine(px[(start[i]+angle)-360],py[(start[i]+angle)-360],lx[(start[i]+angle)-360],ly[(start[i]+angle)-360],color1);
-    }
+        sprite1.drawString( cc[i]
+                          , x[start[i]+angle]
+                          , y[start[i]+angle] );
+        
+        sprite1.drawLine( px[start[i]+angle]
+                        , py[start[i]+angle]
+                        , lx[start[i]+angle]
+                        , ly[start[i]+angle]
+                        , color1 );
+     }
+     else
+     {
+        sprite1.drawString( cc[i]
+                          , x[(start[i]+angle)-360]
+                          , y[(start[i]+angle)-360] );
+
+        sprite1.drawLine( px[(start[i]+angle)-360]
+                        , py[(start[i]+angle)-360]
+                        , lx[(start[i]+angle)-360]
+                        , ly[(start[i]+angle)-360]
+                        , color1 );
+     }
+  }
 
   float v = angle/3.60;
-  for(int i=0; i<60; i++)
-  
-  if(startP[i] + angle<360)
-      sprite1.fillCircle(px[startP[i]+angle], py[startP[i]+angle], 1, color1);  //--- TFT_WHITE
-  else
-      sprite1.fillCircle(px[(startP[i]+angle)-360], py[(startP[i]+angle)-360], 1, color1);
 
-  sprite1.setTextColor(TFT_SILVER, color6);
-  sprite1.drawString("AMBIENT", sx, sy-22, 2);
-  sprite1.drawString("LIGHT"  , sx, sy+18, 2);
-  sprite1.setTextColor(color1, color6);
-  sprite1.drawString(String((int)v)+"%",sx,sy,4);
-  sprite1.fillTriangle(70,28,74,36,66,36,TFT_RED);
+  for(int i=0; i<60; i++)
+  {
+    if(startP[i] + angle<360)
+        sprite1.fillCircle( px[startP[i]+angle]
+                          , py[startP[i]+angle]
+                          , 1
+                          , color1);  //--- TFT_WHITE
+    else
+        sprite1.fillCircle( px[(startP[i]+angle)-360]
+                          , py[(startP[i]+angle)-360]
+                          , 1
+                          , color1);  //--- TFT_WHITE
+  }
+  //--- update gauge inner text area
+  sprite1.setTextColor(TFT_SILVER, color6);   //--- light_grey
+  sprite1.drawString( "AMBIENT"
+                    , sx
+                    , sy-22
+                    , 2 );
+
+  sprite1.drawString( "LIGHT"  
+                    , sx
+                    , sy+18
+                    , 2 );  //--- font
+
+  sprite1.setTextColor(color1, color6);  //---fg,bg
+
+  sprite1.drawString( String( (int)v) + "%"
+                    , sx
+                    , sy
+                    , 4 ); //---font
+
+  sprite1.fillTriangle( 70   //x1
+                      , 28   //y1
+                      , 74   //x2
+                      , 36   //y2
+                      , 66   //x3
+                      , 36   //y3
+                      , TFT_RED );
  
   //--- graph @ sprite2
-  for(int i=1;i<10;i++)
-    sprite2.drawLine(gx+(i*gw/10),gy,gx+(i*gw/10),gy-gh,color2);
-  
-  //--- graph: y-axis labels and x-axis
-  for(int i=1;i<6;i++)
+  for(int i=1; i<10; i++)
   {
-    sprite2.drawLine(gx,gy-(i*9),gx+gw,gy-(i*9),color2);
-  
-    if(i==1 || i==3 || i==5)
-      sprite2.drawString(String(i*9-27),gx-12,gy-(i*9));
-    }
+    sprite2.drawLine( gx+(i*gw/10)
+                    , gy
+                    , gx+(i*gw/10)
+                    , gy-gh
+                    , color2 );
+  }
+
+  //--- graph: y-axis labels and x-axis
+  for(int i=1; i<6; i++)
+  {
+      sprite2.drawLine( gx
+                      , gy-(i*9)
+                      , gx+gw
+                      , gy-(i*9)
+                      , color2 );
+    
+      if(i==1 || i==3 || i==5)
+      {   
+          sprite2.drawString( String(i*9-27)
+                            , gx-12
+                            , gy-(i*9) );
+      }
+  }
    
   //--- draw y-axis with emphasis
-  sprite2.drawLine(gx,gy,gx,gy-gh,TFT_WHITE);
+  sprite2.drawLine( gx
+                  , gy
+                  , gx
+                  , gy-gh
+                  , TFT_WHITE);
 
   //--- draw value-data
   for(int i=0;i<19;i++)
   {
-    sprite2.drawLine(gx+(i*8), gy-values[i]-calib,   gx+((i+1)*9), gy-values[i+1]-calib,   TFT_RED);
-    sprite2.drawLine(gx+(i*8), gy-values[i]-1-calib, gx+((i+1)*8), gy-values[i+1]-1-calib, TFT_RED);
+      sprite2.drawLine( gx+(i*8)
+                      , gy-values[i]-calib
+                      , gx+((i+1)*9)
+                      , gy-values[i+1]-calib
+                      , TFT_RED);
+                    
+      sprite2.drawLine( gx+(i*8)
+                      , gy-values[i]-1-calib
+                      , gx+((i+1)*8)
+                      , gy-values[i+1]-1-calib
+                      , TFT_RED );
   }
 
-  //--- Terminal w. Sprite 3
+  //--- terminal @ Sprite 3
   if(curentTime+period<millis())
   {
     sprite3.fillSprite(TFT_BLACK);
+
     c1=c;
+
     for(int i=0;i<6;i++)
     {
-      term2[i]= term[i];
-      t2[i]   = t[i];
+      term2[i] = term[i];
+      t2[i]    = t[i];
     }
 
-    for(int i=5;i>0;i--)
+    for(int i=5; i>0; i--)
     {
       term[i-1] = term2[i];
       t[i-1]    = t2[i];
     }
 
     //--- terminal@sprite3  @ pin 4 adc !
-    term[5]=(analogRead(4) * 2 * 3.3 * 1000) / 4096;
-    t[5]=number;
+    term[5] = (analogRead(4) * 2 * 3.3 * 1000) / 4096;
+    t[5]    = number;
     sprite3.setTextColor(TFT_WHITE,color5);
     
     for(int i=0;i<6;i++)
     {
-      sprite3.drawString(String(t[i])+"  BATERY="+String(term[i])+"     ",4,4+i*11);
-      c1=!c1;
-    
+        sprite3.drawString( String(t[i]) + "  BATT=" + String(term[i]) + "     "
+                            , 4     //x
+                            , 4 + i * 11 ); //y
+        c1=!c1;
     }
     curentTime=millis();
     c=!c;
     number++;
   }
-//--- label_on_off@sprite4
+  //--- button_label @ sprite4
   if(onOff==1)
-    sprite4.drawString("ON", 20,10);
+      sprite4.drawString("ON", 20,10);
   else
-    sprite4.drawString("OFF",20,10);
+      sprite4.drawString("OFF",20,10);
 
   //sprite4.drawCircle(20,28,10,TFT_SILVER);
   //sprite4.drawCircle(20,64,10,TFT_SILVER);
   //sprite4.drawRect(10,28,21,36,TFT_SILVER);
   //sprite4.fillRect(11,28,19,36,color3);
 
-  sprite4.drawCircle(120,28,10,TFT_SILVER);
+  sprite4.drawCircle(120,28,10,TFT_SILVER);   //x,y,r,color
   sprite4.drawCircle(120,64,10,TFT_SILVER);
   sprite4.drawRect(110,28,21,20,TFT_SILVER);
   sprite4.fillRect(111,28,19,20,color3);
@@ -438,6 +512,6 @@ void loop()
   sprite1.pushSprite(0,20);   //--- gauge
   sprite2.pushSprite(140,70); //--- graph
   sprite3.pushSprite(80,160); //--- terminal
-  sprite4.pushSprite(40,180);  //--- text_on_off
+  sprite4.pushSprite(40,180); //--- text_on_off
  
 }

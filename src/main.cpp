286c132618245c2b835c6e2310be67d96e894d5c
juergs <juergen.schweiss@web.de>
2024-04-21 17:12:26 +0200

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,32 @@
 
   https://medium.com/@manuel.bl/low-cost-esp32-in-circuit-debugging-dbbee39e508b
 
+  Attention this code was intended to use a normal rectangular 240#240 Display, not round. 
+  The GC9A01 diplay is roung, there are som pixel accessible in memory, but not visible
+
+  *** Define ALTERNATE_PINS to use non-standard GPIO pins for SPI bus
+#ifdef ALTERNATE_PINS
+  #define VSPI_MISO   2
+  #define VSPI_MOSI   4
+  #define VSPI_SCLK   0
+  #define VSPI_SS     33
+
+  #define HSPI_MISO   26
+  #define HSPI_MOSI   27
+  #define HSPI_SCLK   25
+  #define HSPI_SS     32
+#else
+  #define VSPI_MISO   MISO
+  #define VSPI_MOSI   MOSI
+  #define VSPI_SCLK   SCK
+  #define VSPI_SS     SS
+
+  #define HSPI_MISO   12
+  #define HSPI_MOSI   13
+  #define HSPI_SCLK   14
+  #define HSPI_SS     15
+#endif
+
 */
 
 
@@ -85,6 +111,19 @@ TFT_eSprite sprite4   = TFT_eSprite(&lcd);
 #define color4 0xF2DF
 #define color5 0x00A3
 #define color6 0x00A3
+
+// Color definitions - in 5:6:5
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0 
+#define WHITE           0xFFFF
+#define TEST            0x1BF5
+#define JJCOLOR         0x1CB6
+#define JJORNG          0xFD03
 
 /* 
   int ADC1 = 1; //light sensor pin
@@ -137,7 +176,6 @@ const int textHeight = 30;
 const int arrowHeight = distanceOffset - textHeight;
 
 //----------------------------------------------------
-
 void drawStringWithColor(int angle, String text, uint16_t color) 
 {
   lcd.setPivot(mx, my);
@@ -150,9 +188,7 @@ void drawStringWithColor(int angle, String text, uint16_t color)
   spr.drawString(text, 32, 15, 4);
   spr.pushRotated(angle);
 } 
-
 //----------------------------------------------------
-
 void setup() 
 {
     pinMode(BTN,INPUT_PULLUP);
@@ -196,20 +232,19 @@ void setup()
     lcd.setTextColor(TFT_ORANGE,TFT_BLACK);    //--- fg, bg
     lcd.drawString("  ADC READ  ",120,10,2);  //--- text x,y,font. direct to display
     lcd.setTextColor(TFT_ORANGE,TFT_BLACK);
-    lcd.drawString("MICRO",170,35,2);
+    lcd.drawString("MICRO",170,35,2);     //--- Labe, graph headline
     lcd.drawString("MINI TERMINAL",200,150);  //---Ueberschrift TextTerminal wird direkt geschrieben
 
-    sprite1.createSprite(140,140);
+    sprite1.createSprite(140,140);      //--- text in gauge
     sprite1.setTextDatum(4);   //--text position reference
 
-    sprite2.createSprite(150,55);
+    sprite2.createSprite(150,55);   //--- graph
     sprite2.setTextDatum(4);
 
-    sprite3.createSprite(120,70);
-    //sprite4.createSprite(40,80);
+    sprite3.createSprite(120,70);   //--- termnal with text
     sprite3.fillSprite(color3);
 
-    sprite4.createSprite(40,80);  
+    sprite4.createSprite(40,80);      //--- label forr on/off button
     sprite4.fillSprite(TFT_BLACK);
     sprite4.setTextDatum(4);
     sprite4.setTextColor(color1,color3);
